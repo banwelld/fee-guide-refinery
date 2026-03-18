113357157f4c09cbcc2060d908cb5199cd1fad05
Dave Banwell <dave@davebanwell.com>
2026-03-17 22:47:45 -0400

    build provinces and specialties onbects

---
+++ client/src/features/extraction/config/specialties.js
@@ -0,0 +1,12 @@
+const SPECIALTIES = [
+  { code: 'GEN', name: 'General Practice' },
+  { code: 'PUB', name: 'Public Health' },
+  { code: 'ENDO', name: 'Endodontics' },
+  { code: 'ORTHO', name: 'Orthodontics' },
+  { code: 'PED', name: 'Pediatrics' },
+  { code: 'PERIO', name: 'Periodontics' },
+  { code: 'PROS', name: 'Prosthodontics' },
+  { code: 'OMFS', name: 'Oral and Maxillofacial Surgery' },
+];
+
+export default SPECIALTIES;
